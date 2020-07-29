95d6a11a59b12236c5bd4f8029e5b958bfcbd250
Hejdasz <68949223+Hejdasz@users.noreply.github.com>
2020-07-29 16:07:44 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,25 +1,20 @@
 f = rarfile.RarFile(p / 'test.rar')
-
 rf.testrar()                                #extract rar
-
 rf.extractall(d)
 rf.close()
 
-
-
-exampleZip.extractall(p)                   '''extract zip'''
+exampleZip.extractall(p)                     #extract zip
 exampleZip.close()
 
-print(exampleZip.namelist())                '''show files in zip'''
+
+print(exampleZip.namelist())                 #show files in zip
 
 
 for f in rf.infolist():
     Fname = f.filename
-    FnameSplit = Fname.split()                  #show files in rar
+    FnameSplit = Fname.split()               #show files in rar
     print(FnameSplit[-1])
 
 
 
-
-
-mylist = [f for f in glob.glob("*.rar")]    #list of all files in dir
+mylist = [f for f in glob.glob("*.rar")]     #list of all files in dir
