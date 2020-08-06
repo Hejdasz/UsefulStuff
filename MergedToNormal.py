63e8d8cdbe924c97e9155ba92fe9de739f3359d8
Hejdasz <68949223+Hejdasz@users.noreply.github.com>
2020-08-06 13:36:01 +0200

    Create MergedToNormal.py

---
+++ MergedToNormal.py
@@ -0,0 +1,20 @@
+import os, re, pprint, openpyxl
+from datetime import time , datetime ,timedelta
+
+
+os.chdir('H:\\work\\test')
+
+wb = openpyxl.load_workbook('example.xlsx')
+sheet = wb['1']
+
+classes = {}
+
+for i in range(2,1214):                   
+                       
+            if sheet.cell(row=int(i), column=1).value == None:
+                sheet.cell(row=int(i), column=1).value = sheet.cell(row=int(i-1), column=1).value
+                
+                
+
+wb.save('example.xlsx')
+print("\n"+"end")
