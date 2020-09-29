e5bcaf57bba41ad34316eb647671ed43d6ba9812
Hejdasz <68949223+Hejdasz@users.noreply.github.com>
2020-09-29 15:54:24 +0200

    Update ExcelToVideos.py

--- ExcelToVideos.py
+++ ExcelToVideos.py
@@ -1,24 +1,50 @@
 import os, re, pprint, openpyxl
+import shutil 
 
-os.chdir('H:\\work\\test')
+os.chdir('F:\\test')
 wb = openpyxl.load_workbook('example.xlsx')
-sheet = wb['1']
+sheet = wb['4']
+
+row_count = sheet.max_row
 classes = []
-for i in range(2,1215):
-    Page = sheet.cell(row=int(i), column=1).value
+pages = []
+for i in range(2,row_count+1):
+    Page = sheet.cell(row=int(i), column=2).value
     number = sheet.cell(row=int(i), column=3).value
-    classesObject = str(Page)+' ('+str(number)+')'
+    pages.append(number)
+    classes.append(Page)
+b = 0
+'''	
+for a in classes:
+	print(a)
+	b+=1
 
-    classes.append(classesObject)
+print(b)
+'''
 
+print(len(classes))
+print(len(pages))
+
+xd = input()
 a =0
-for folderName, subfolders, filenames in os.walk('H:\\work\\test\\more test'):
-    for filename in filenames:
-        print(filename)
-        old_name = (folderName+'\\'+filename)
-        
-        os.rename(old_name, folderName+'\\'+classes[a]+'.txt')
-        a+=1
-        
-        
-    break
+
+for folderName, subfolders, filenames in os.walk('F:\\test\\to sort'):
+	
+	for filename in filenames:
+
+		print(filename)
+		print(classes[a])
+		print()					
+		old_name = (folderName+'\\'+filename)
+
+		new_name = folderName+'\\'+classes[a]+'.mp4'
+		os.rename(old_name,new_name)		
+		dest = pages[a]
+		print(dest)		
+		dir = os.path.join('F:\\test\\sorted',dest)		
+		if os.path.exists(dir) == True:
+			shutil.move(new_name,dir)
+		else:
+			os.mkdir(dir)
+			shutil.move(new_name,dir)		
+		a+=1
